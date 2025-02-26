0bbe98c1075213d5004db45827457ca2fdce98f9
Vladimir Kharitonov <Vladimir.Kharitonov@jetbrains.com>
2025-02-26 18:02:53 +0100

    add time measurement

--- skiko/src/awtMain/kotlin/org/jetbrains/skiko/swing/MetalSwingRedrawer.kt
+++ skiko/src/awtMain/kotlin/org/jetbrains/skiko/swing/MetalSwingRedrawer.kt
@@ -77,6 +77,7 @@ internal class MetalSwingRedrawer(
                 flush(surface, g)
             }
         }
+        profiler.printEvery10Sec()
     }
 
     private fun flush(surface: Surface, g: Graphics2D) {
